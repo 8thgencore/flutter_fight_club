e2e35607016ea9ccd22f1a5b9e1429c225560280
8thgencore <30128845+8thgencore@users.noreply.github.com>
2021-06-05 01:10:10 +0300

    lesson code

--- lib/main.dart
+++ lib/main.dart
@@ -22,51 +22,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  BodyPart? defendingBodyPart;
+  BodyPart? attackingBodyPart;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-
         children: [
           SizedBox(height: 40),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text("You"),
-              Text("Enemy"),
+              SizedBox(height: 16),
+              Expanded(child: Center(child: Text("You"))),
+              SizedBox(height: 12),
+              Expanded(child: Center(child: Text("Enemy"))),
+              SizedBox(height: 16),
             ],
           ),
+          Expanded(child: SizedBox()),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(width: 16),
               Expanded(
-                child: ColoredBox(
-                  color: Colors.tealAccent[100]!,
-                  child: Column(
-                    children: [
-                      Text("Defend".toUpperCase()),
-                      SizedBox(height: 13),
-                      BodyPartButton(text: "Head"),
-                      SizedBox(height: 14),
-                      BodyPartButton(text: "Torso"),
-                    ],
-                  ),
+                child: Column(
+                  children: [
+                    Text("Defend".toUpperCase()),
+                    SizedBox(height: 13),
+                    BodyPartButton(
+                      bodyPart: BodyPart.head,
+                      selected: defendingBodyPart == BodyPart.head,
+                      bodyPartSetter: _selectDefendingBodyPart,
+                    ),
+                    SizedBox(height: 14),
+                    BodyPartButton(
+                      bodyPart: BodyPart.torso,
+                      selected: defendingBodyPart == BodyPart.torso,
+                      bodyPartSetter: _selectDefendingBodyPart,
+                    ),
+                  ],
                 ),
               ),
               SizedBox(width: 12),
               Expanded(
-                child: ColoredBox(
-                  color: Colors.deepOrangeAccent[100]!,
-                  child: Column(
-                    children: [
-                      Text("Attack".toUpperCase()),
-                      SizedBox(height: 13),
-                      BodyPartButton(text: "Head"),
-                      SizedBox(height: 14),
-                      BodyPartButton(text: "Torso"),
-                    ],
-                  ),
+                child: Column(
+                  children: [
+                    Text("Attack".toUpperCase()),
+                    SizedBox(height: 13),
+                    BodyPartButton(
+                      bodyPart: BodyPart.head,
+                      selected: attackingBodyPart == BodyPart.head,
+                      bodyPartSetter: _selectAttackingBodyPart,
+                    ),
+                    SizedBox(height: 14),
+                    BodyPartButton(
+                      bodyPart: BodyPart.torso,
+                      selected: attackingBodyPart == BodyPart.torso,
+                      bodyPartSetter: _selectAttackingBodyPart,
+                    ),
+                  ],
                 ),
               ),
               SizedBox(width: 16),
@@ -97,29 +112,64 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               SizedBox(width: 16),
             ],
-          )
+          ),
+          SizedBox(height: 40),
         ],
       ),
     );
   }
+
+  void _selectDefendingBodyPart(final BodyPart value) {
+    setState(() {
+      defendingBodyPart = value;
+    });
+  }
+
+  void _selectAttackingBodyPart(final BodyPart value) {
+    setState(() {
+      attackingBodyPart = value;
+    });
+  }
+}
+
+class BodyPart {
+  final String name;
+
+  const BodyPart._(this.name);
+
+  static const head = BodyPart._("Head");
+  static const torso = BodyPart._("Torso");
+
+  @override
+  String toString() {
+    return 'BodyPart{name: $name}';
+  }
 }
 
 class BodyPartButton extends StatelessWidget {
-  final String text;
+  final BodyPart bodyPart;
+  final bool selected;
+  final ValueSetter<BodyPart> bodyPartSetter;
 
   const BodyPartButton({
     Key? key,
-    required this.text,
+    required this.bodyPart,
+    required this.selected,
+    required this.bodyPartSetter,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 40,
-      child: ColoredBox(
-        color: Colors.black26,
-        child: Center(
-          child: Text(text.toUpperCase()),
+    return GestureDetector(
+      onTap: () => bodyPartSetter(bodyPart),
+      child: SizedBox(
+        height: 40,
+        child: ColoredBox(
+          color:
+              selected ? const Color.fromRGBO(28, 121, 206, 1) : Colors.black26,
+          child: Center(
+            child: Text(bodyPart.name.toUpperCase()),
+          ),
         ),
       ),
     );
